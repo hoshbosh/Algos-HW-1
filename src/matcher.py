880da08596e48a297465303fac34bf49f3d2f7cc
ashamiller04 <ashamiller@ufl.edu>
2026-01-30 14:30:09 -0500

    added stableMatching function

--- src/matcher.py
+++ src/matcher.py
@@ -1,2 +1,28 @@
+def tradeUP(preferences, student, hospital, hospital1):
 
-print("matcher")
+
+
+def stableMatching(preferences):
+    n = len(preferences['hospitals'])
+    unmatchedCount = n
+    studentsH = [-1] * n
+    hospitalsUM = [False] * n
+
+    while unmatchedCount > 0:
+        hospital = next(i for i in range(n) if not hospitalsUM[i])
+        for i in range(n):
+            if hospitalsUM[i]:
+                break
+
+            student = preferences[hospital][i]
+            if studentsH[student - n] == -1:
+                studentsH[student - n] = hospital
+                hospitalsUM[hospital] = True
+                unmatchedCount -= 1
+            else:
+                hospital1 = studentsH[student - n]
+                if not tradeUP(preferences, student, hospital, hospital1):
+                    studentsH[student - n] = hospital
+                    hospitalsUM[hospital] = True
+                    hospitalsUM[hospital1] = False
+    return studentsH
