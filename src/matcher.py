14eb1e7e11a7fe00bd04dfe24107df5cc8a61c36
ashamiller04 <ashamiller@ufl.edu>
2026-01-30 16:10:27 -0500

    Updated stableMatching function

--- src/matcher.py
+++ src/matcher.py
@@ -1,28 +1,34 @@
-def tradeUP(preferences, student, hospital, hospital1):
-
-
+import get_input
 
 def stableMatching(preferences):
-    n = len(preferences['hospitals'])
-    unmatchedCount = n
-    studentsH = [-1] * n
-    hospitalsUM = [False] * n
+    n = len(preferences["preferences"]["hospitals"])
+    free = [x for x in range(n)]
+    matches = [None] * n
 
-    while unmatchedCount > 0:
-        hospital = next(i for i in range(n) if not hospitalsUM[i])
-        for i in range(n):
-            if hospitalsUM[i]:
+    while len(free) > 0:
+
+        curr = free.pop(0)
+        curr_prefs = preferences['preferences']['hospitals'][curr]
+
+
+        for student in curr_prefs:
+            matched = student in matches
+            student_curr_prefs = preferences['preferences']['students'][student]
+            if not matched:
+                matches[student] = curr
                 break
 
-            student = preferences[hospital][i]
-            if studentsH[student - n] == -1:
-                studentsH[student - n] = hospital
-                hospitalsUM[hospital] = True
-                unmatchedCount -= 1
+
+            elif student_curr_prefs.index(curr) < student_curr_prefs.index(matches.index(student)):
+                matches[matches.index(student)] = None
+                matches[student] = curr
             else:
-                hospital1 = studentsH[student - n]
-                if not tradeUP(preferences, student, hospital, hospital1):
-                    studentsH[student - n] = hospital
-                    hospitalsUM[hospital] = True
-                    hospitalsUM[hospital1] = False
-    return studentsH
+                continue
+
+
+    return matches
+
+
+
+#testing
+print(stableMatching(get_input.get_inputs('example.in')))
