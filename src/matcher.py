693fc1d45bbe76fd8fff75128ce9833be4052e4b
ashamiller04 <ashamiller@ufl.edu>
2026-01-30 19:45:22 -0500

    Fixed an indexing issue in stableMatching

--- src/matcher.py
+++ src/matcher.py
@@ -8,7 +8,7 @@ def stableMatching(preferences):
 
     while len(free) > 0:
 
-        curr = free.pop(0)
+        curr = free[0]
         curr_prefs = preferences['preferences']['hospitals'][curr]
 
 
@@ -17,14 +17,17 @@ def stableMatching(preferences):
             student_curr_prefs = preferences['preferences']['students'][student]
             if not matched:
                 matches[curr] = student
+                free.pop(0)
                 break
 
 
             elif student_curr_prefs.index(curr) < student_curr_prefs.index(matches.index(student)):
                 replaced = matches.index(student)
                 matches[curr] = student
-                matches[matches.index(student)] = None
+                matches[replaced] = None
+                free.pop(0)
                 free.append(replaced)
+                break
             else:
                 continue
 
@@ -41,5 +44,5 @@ def matchingOutput(preferences):
 
 
 #for local testing purposes
-#print(stableMatching(get_input.get_inputs('example.in')))
-matchingOutput(get_inputs("n_20.in"))
+#print(stableMatching(get_inputs('n_200.in')))
+matchingOutput(get_inputs("n_400.in"))
